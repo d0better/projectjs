b40222286c37ff295fe4c53ca8bb0708f5f579f7
d0better <konomoses23@gmail.com>
2026-06-24 15:38:38 +0100

    determine even or odd number

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-let username = prompt("input username", "");
-if (username === "admin") {
-    let pass = prompt ("password", "")
-    if (pass === "kingpinto12") {
-        alert("welcome")
-    } else if (pass === "") {
-        alert("cancelled")
-    } else {(pass !== "kingpinto12") 
-        alert("wrong password")
-    }
-}
-if (username === "") {
-    alert("cancel")
-} else {(username !== "amdin") 
-    alert("wrong user")
-}
+let number = 50;
+if (number % 2 == 0) {
+    alert("the number is even")
+} else (alert("number is odd"))
