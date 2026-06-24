0b265033f922851014b2d0ecb9d35a30c093e6b5
d0better <konomoses23@gmail.com>
2026-06-24 16:28:11 +0100

    count 1 to 10 using loop

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-let num = Number(prompt("input number", ""));
-if (num > 0) {
-    alert ("positive")
-} else if (num < 0) {
-    alert("negative")
-} else {
-    alert(0)
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+    
 }
