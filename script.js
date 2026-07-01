03c072bdd56769de450da3f3e1e9d95ab2001326
d0better <konomoses23@gmail.com>
2026-07-01 11:31:01 +0100

    using function to checkage

--- script.js
+++ script.js
@@ -1,49 +1,13 @@
-let pin =Number(prompt("input pin", ""))
-if (pin === 1234) {
-    let choice = Number(prompt("choose an option : \n1 - check balance \n2 - deposit \n3 - withdraw \n4 - Even or Odd \n5 - count to a number  " ));
-     let balance = Number(50000);
-     let total;
-     let newbalance;
-    switch (choice) {
-        case 1:
-            alert("your balance is $ " + balance)
-            break;
-        case 2:
-            let deposit = Number(prompt("how much do you want to deposit", ""));
-            total = Number (deposit + balance);
-            alert("your new balance is $" + total)
-                break;
-        case 3:
-            let withdraw = Number (prompt("how much do you want to withdraw"))
-            if (withdraw > balance) {
-                alert("insufficient balance")
-            } else {
-                  newbalance = Number(balance - withdraw);
-                  alert("your newbalance is " + newbalance) 
-            }
-            break;
-        case 4 :
-            let num = Number(prompt("input number"));
-            if (num % 2 === 0) {
-                alert ("Even number")
-            } else{
-                alert("Odd Number")
-            }
-            break;
-        case 5:
-            let count = Number(prompt("count to what Number?", ""));
-            for (let i = 1; i <= count; i++) {
-                if (i % 2 === 0) {
-                    console.log (i + " is an even number")
-                }  else{
-                    console.log(i + " is an odd number")
-                }
-            }
-            break;
-        default:
-            alert("invalid option")
-            break;
+function checkAge(age) {
+    if (age >= 18) {
+        return true;
+    } else{
+        return confirm("do you have permission from your parent?")
     }
-} else{
-    alert("wrong pin")
+}
+let age = prompt("how are you?", 18);
+if (checkAge(age)) {
+    alert("Access Granted")
+} else {
+    alert("Acess denied")
 }
