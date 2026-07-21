327e1918fac524935991b591d8d182a83eff01cf
d0better <konomoses23@gmail.com>
2026-07-21 10:05:50 +0100

    learning array methods (find and findIndex)

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function pow(x, n) {
-    let result = x
-    for (let i = 1; i < n ; i++) {
-        result *= x
-    }
-    return result;
-}
-let x = prompt("x?", "");
-let n = prompt("n?", "");
-if (n < 1) {
-    alert(`power ${n} is not supported , use a positive integer`);
-} else {
-    alert(pow(x, n))
-}
+let users = [
+    {name: "John", age: 18},
+    {name: "Sarah", age: 25},
+    {name: "Mike", age: 30}
+];
+let user = users.find(function(person) {
+    return person.age > 20 ;
+});
+let index = users.findIndex(function(person) {
+    return person.age > 20;
+});
+console.log(user);
+console.log(index);
