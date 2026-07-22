824459e6197bad2b76eeae815434a0b03bd0011b
d0better <konomoses23@gmail.com>
2026-07-22 11:29:21 +0100

    learning array method (split and join)

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-let lenghts = ["bilbo", "gandalf","Nazgul"].map(item => item.length);
-console.log(lenghts)
-
-let prices = [10, 20, 30]
-let doubled = prices.map(function(price) {
-    return price * 2
-});
-console.log(doubled);
+let names = "Tayo, Ayo, Moyo"
+let arr = names.split(",").join(" and ")
+alert(arr)
